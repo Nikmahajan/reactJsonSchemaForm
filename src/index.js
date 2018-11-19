71faeb08f75e8b2a9227e2f100c350ea7ba1fa78
Nikhil Mahajan <nikhil.mahajan@pepcus.com>
2018-11-19 14:07:29 +0530

    intial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SplashPage from './components/SplashPage';
+
+ReactDOM.render( <SplashPage />, document.getElementById('demo'));
+
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  watch: true,
+  entry: ['babel-polyfill', './src/index.js'],
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css', '.scss'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    })
+  ]
+};
